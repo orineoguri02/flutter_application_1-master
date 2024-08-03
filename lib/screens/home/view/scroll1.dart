5e28a2f727180ffc4c8fb4ebd8e708a3dafb47ad
orineoguri02 <gunwoo5285@gmail.com>
2024-08-03 16:48:36 +0900

    1648

--- lib/screens/home/view/scroll1.dart
+++ lib/screens/home/view/scroll1.dart
@@ -11,24 +11,13 @@ class _CaedscrolState extends State<Cardscrol> {
   @override
   Widget build(BuildContext context) {
     return Container(
-        height: 400,
-        width: 400,
+        height: 350,
+        width: 350,
         // 카드 스크롤의 높이를 설정
         child: ListView(scrollDirection: Axis.horizontal, children: [
           Container(
-            width: 400,
-            height: 400,
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Card(
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(15)),
-                  child: Expanded(child: Image.asset('assets/test1.png'))),
-            ),
-          ),
-          Container(
-            width: 400,
-            height: 400,
+            width: 350,
+            height: 350,
             child: Padding(
               padding: const EdgeInsets.all(8.0),
               child: Card(
@@ -38,8 +27,8 @@ class _CaedscrolState extends State<Cardscrol> {
             ),
           ),
           Container(
-            width: 400,
-            height: 400,
+            width: 350,
+            height: 350,
             child: Padding(
               padding: const EdgeInsets.all(8.0),
               child: Card(
@@ -49,8 +38,19 @@ class _CaedscrolState extends State<Cardscrol> {
             ),
           ),
           Container(
-            width: 400,
-            height: 400,
+            width: 350,
+            height: 350,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Card(
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(15)),
+                  child: Image.asset('assets/test1.png')),
+            ),
+          ),
+          Container(
+            width: 350,
+            height: 350,
             child: Padding(
               padding: const EdgeInsets.all(8.0),
               child: Card(
